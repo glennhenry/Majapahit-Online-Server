2deffedcb69509fef012e810051682104eb0dd7e
glennhenry <glennhenrybdg05@gmail.com>
2026-06-04 14:37:15 +0700

    Properly handle registration

--- src/main/kotlin/game/routes/handler/SimpleRegister.kt
+++ src/main/kotlin/game/routes/handler/SimpleRegister.kt
@@ -6,10 +6,13 @@ import game.routes.models.pio.auth.SimpleRegisterArgs
 import game.routes.models.pio.auth.SimpleRegisterOutput
 import encore.context.ServerContext
 import encore.fancam.Fancam
+import encore.security.Screening
 import encore.serialization.Protobuf
 import encore.utils.types.isFail
 import encore.utils.types.okOrThrow
+import game.routes.utils.withErrorHeader
 import game.routes.utils.withSuccessHeader
+import io.ktor.http.content.ByteArrayContent
 import io.ktor.server.request.receiveChannel
 import io.ktor.server.response.respondBytes
 import io.ktor.server.routing.*
@@ -25,6 +28,50 @@ suspend fun RoutingContext.simpleRegister(serverContext: ServerContext) {
         call.receiveChannel().toByteArray()
     )
 
+    var failedRegisterResponse: ByteArray? = null
+    Screening("Verify register")
+        .check(
+            description = "Email non-blank",
+            predicate = { simpleRegisterArgs.email.isNotBlank() },
+            onFail = { failedRegisterResponse = handleFailedRegistration(3) }
+        )
+        .check(
+            description = "Email contains '@'",
+            predicate = { simpleRegisterArgs.email.contains("@") },
+            onFail = { failedRegisterResponse = handleFailedRegistration(3) }
+        )
+        .checkSuspend(
+            description = "Email is unique",
+            predicate = { serverContext.subunits.auth.isEmailAvailable(simpleRegisterArgs.email).okOrThrow() },
+            onFail = { failedRegisterResponse = handleFailedRegistration(3) }
+        )
+        .check(
+            description = "Username non-blank",
+            predicate = { simpleRegisterArgs.username.isNotBlank() },
+            onFail = { failedRegisterResponse = handleFailedRegistration(1) }
+        )
+        .check(
+            description = "Username does not contain bad words",
+            predicate = { !simpleRegisterArgs.username.contains("dick") },
+            onFail = { failedRegisterResponse = handleFailedRegistration(1) }
+        )
+        .checkSuspend(
+            description = "Username is unique",
+            predicate = { serverContext.subunits.auth.isUsernameAvailable(simpleRegisterArgs.username).okOrThrow() },
+            onFail = { failedRegisterResponse = handleFailedRegistration(1) }
+        )
+        .check(
+            description = "Password length 5 length at minimum",
+            predicate = { simpleRegisterArgs.password.length > 5 },
+            onFail = { failedRegisterResponse = handleFailedRegistration(2) }
+        )
+
+    // which mean one of the check failed
+    if (failedRegisterResponse != null) {
+        call.respondBytes(failedRegisterResponse)
+        return
+    }
+
     val registrationResult = serverContext.subunits.auth.register(
         simpleRegisterArgs.username,
         simpleRegisterArgs.password,
@@ -33,7 +80,6 @@ suspend fun RoutingContext.simpleRegister(serverContext: ServerContext) {
 
     if (registrationResult.isFail()) {
         Fancam.error { "Registration failed on server..." }
-        // [unimplemented] handle failed registration... use PlayerIORegistrationError
         return
     }
 
@@ -42,7 +88,7 @@ suspend fun RoutingContext.simpleRegister(serverContext: ServerContext) {
         token = session.token,
         userId = session.userId,
         showBranding = true,
-        gameFSRedirectMap = "1",
+        gameFSRedirectMap = "",
         partnerId = "",
         playerInsightState = PlayerInsightState()
     )
@@ -53,17 +99,53 @@ suspend fun RoutingContext.simpleRegister(serverContext: ServerContext) {
 }
 
 @OptIn(ExperimentalSerializationApi::class)
-private fun handleFailedRegistration(): ByteArray {
-    fun errorFor(field: String): String {
-        return "$field error"
+private fun handleFailedRegistration(whatError: Int): ByteArray {
+    val errorOutput = when (whatError) {
+        1 -> {
+            PlayerIORegistrationError(
+                usernameError = "Username error",
+                passwordError = "",
+                emailError = "",
+                captchaError = ""
+            )
+        }
+
+        2 -> {
+            PlayerIORegistrationError(
+                usernameError = "",
+                passwordError = "Password error",
+                emailError = "",
+                captchaError = ""
+            )
+        }
+
+        3 -> {
+            PlayerIORegistrationError(
+                usernameError = "",
+                passwordError = "",
+                emailError = "Email error",
+                captchaError = ""
+            )
+        }
+
+        4 -> {
+            PlayerIORegistrationError(
+                usernameError = "",
+                passwordError = "",
+                emailError = "",
+                captchaError = "Captcha error"
+            )
+        }
+
+        else -> {
+            PlayerIORegistrationError(
+                usernameError = "",
+                passwordError = "",
+                emailError = "",
+                captchaError = ""
+            )
+        }
     }
 
-    val errorOutput = PlayerIORegistrationError(
-        usernameError = errorFor("Username"),
-        passwordError = errorFor("Password"),
-        emailError = errorFor("Email"),
-        captchaError = ""
-    )
-
-    return Protobuf.encode(errorOutput)
+    return Protobuf.encode(errorOutput).withErrorHeader()
 }
